6745fb4a7648562688d76d118843cddf88baf119
Koras02 <wjdgh0727@gmail.com>
2021-01-11 21:00:44 +0900

    footer

--- src/MainPage /Footer/Footer.js
+++ src/MainPage /Footer/Footer.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import {FooterWrap,FooterLeft,FooterLeftH1,FooterLeftP,FooterRight,FooterRightH1,FooterRightP} from './FooterStlye'
+import {FooterWrap,FooterHeaderH1,FooterHeaderP} from './FooterStlye'
 
 function Footer() {
      return (
        <FooterWrap>
-           <FooterLeft>
-               <FooterLeftH1>MyPortfolio Blog</FooterLeftH1>
-               <FooterLeftP>Tistory</FooterLeftP>
-               <FooterLeftP>Copyright @ KaKao Corp. All rights reserved.</FooterLeftP>
-           </FooterLeft>
-           <FooterRight> 
-               <FooterRightH1>블로그 소개</FooterRightH1>
-               <FooterRightP>사용언어</FooterRightP>
-               <FooterRightP>웹개발 배우기</FooterRightP>
-           </FooterRight>
+        <FooterHeaderH1>Copyright @ Kakao Corp.Allrights reserved</FooterHeaderH1>
+        <FooterHeaderP>관련사이트</FooterHeaderP>
        </FooterWrap>
      );
  }
--- src/MainPage /Footer/FooterStlye.js
+++ src/MainPage /Footer/FooterStlye.js
@@ -1,53 +1,29 @@
 import styled from 'styled-components';
 
 export const FooterWrap = styled.div`
-  width:96%;
-  margin: 0 auto;
+  width:97.8%;
+  margin: auto;
   height:220px;
   position:relative;
-  left:1%;
+  padding-left:2px;
   float:left;
   padding:20px;
   border:1px solid #000;
 `;
 
-export const FooterLeft = styled.div`
-  width:50%;
-  border:1px solid #000;
-`;
-
-
-export const FooterLeftP = styled.div`
-  font:bold 18px/24px arial;
-  float:left;
-`
-
-export const FooterRight = styled.div`
-  width:50%;
-  float:left;
-  border: 1px solid #000;
-`;
-
-export const FooterLeftH1 = styled.div`
-  font:bold 18px/24px arial;
-  float:left;
-`;
-
  
-
-export const Footerright = styled.div`
-  width:50%; 
-  float:right;
-  border:1px solid #000;
-`;
-
-export const FooterRightH1 = styled.div`
+export const FooterHeaderH1 = styled.div`
+  float:left;
+  padding-top:2%;
+  padding-left:20%;
+  text-algin:left;
   font:bold 18px/24px arial;
-  float:right;
 `;
 
-export const FooterRightP = styled.div`
-   display:inline;
-   float:right;
-   font:bold 16px/18px arial;
-`
+export const FooterHeaderP = styled.div`
+  float:right;
+  padding-top:20px;
+  padding-right:20%;
+  text-align:right;
+  font:bold 20px/24px arial;
+`;
