2d82f7cdc9c33148d3fc974d3dbd0d50fa9c9787
Koras02 <wjdgh0727@gmail.com>
2021-02-10 15:39:41 +0900

    Serviecs 제목 생성

--- src/Pages/Profile/ProfileStyle.js
+++ src/Pages/Profile/ProfileStyle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const ProfileWrapper = styled.div`
   width:100%;
   height:5435px;
-  background:#fff;
+  background:#e3e3e3;
  
   margin-top:0px;
   display:block;
---
+++ src/Pages/Profile/page/Form/Services/ServicesBox.js
@@ -0,0 +1,12 @@
+ import React from 'react';
+
+function ServicesBox() {
+     return (
+       <div>
+         Hello React!
+       </div>
+     );
+ }
+
+
+ export default ServicesBox;
