4c25352e996a4c217d72dd5d826ee973a967935a
Koras02 <wjdgh0727@gmail.com>
2021-02-12 16:43:43 +0900

    Services 항목 완료

--- src/Pages/Profile/page/Form/Services/ServicesForm.js
+++ src/Pages/Profile/page/Form/Services/ServicesForm.js
@@ -1,89 +1,118 @@
- import React, { useState }  from 'react';
-import styled from 'styled-components';
+ import React from 'react';
  
 import { 
   ServicesBox, 
   ServicesForm, 
   // ServicesH1, 
   ServicesWrapper,
-  ImageForm,
+
   Img,
   Description,
+  ImageForm,
+  Icon,
+  Ico,
  
 } from './ServicesFormStyle';
 import './style/Services.scss'
- 
- 
+import {AiFillAlert} from "react-icons/ai";
+
 
 export function ServiceForm () {
-   const [click,setClick] = useState();
-   const onClick = () => {
-     setClick(click => !click);
-   }
-     const onColor = () => {
-       styled.div`
-        colo:red
-       `
-   }
+ 
   return (
         <ServicesForm>
           <ServicesWrapper> 
             <ServicesBox>
-               <ImageForm>
-                  <Img onClick={onClick} Color={onColor}></Img>
-                  <Description>
-                 <h1>WEB DESIGN</h1> 
-                 <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-            </Description>
-               </ImageForm>
-            </ServicesBox>
-            <ServicesBox>
               <ImageForm>
-                  <Img></Img>
-                  <Description>
+              <Img onClick={onchange}>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
+              <Description>
                  <h1>WEB DEVELOPMENT</h1> 
                       <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
             </Description>
-               </ImageForm>
             </ServicesBox>
             <ServicesBox>
-             <ImageForm>
-                 <Img></Img>
-                 <Description>
-                 <h1>PHOTOGRAPHY</h1> 
+            <ImageForm>
+              <Img>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
+              <Description>
+                 <h1>WEB DEVELOPMENT</h1> 
                       <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
             </Description>
-            </ImageForm>
             </ServicesBox>
             <ServicesBox>
-           <ImageForm>
-           <Img></Img>
+            <ImageForm>
+              <Img>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
+             <Description>
+                 <h1>WEB DEVELOPMENT</h1> 
+                      <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+            </Description>
+            </ServicesBox>
+            <ServicesBox>
+            <ImageForm>
+              <Img>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
            <Description>
-                 <h1>RESPONSIV DESIGN</h1> 
+                 <h1>WEB DEVELOPMENT</h1> 
                       <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
             </Description>
-           </ImageForm>
             </ServicesBox>
             <ServicesBox>
-           <ImageForm>
-           <Img></Img>       
+            <ImageForm>
+              <Img>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
            <Description>
-                 <h1>GRAPHIC DESIGN</h1> 
+                 <h1>WEB DEVELOPMENT</h1> 
                       <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
             </Description>
-            </ImageForm>
             </ServicesBox>
             <ServicesBox>
-           <ImageForm>
-           <Img></Img>  
+            <ImageForm>
+              <Img>
+              <Icon>
+                <Ico>
+                 <AiFillAlert size={40} className="Alert" />
+                </Ico>
+              </Icon>
+              </Img>
+              </ImageForm>
            <Description>
-                 <h1>MARKETING SERVICES</h1> 
+                 <h1>WEB DEVELOPMENT</h1> 
                       <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
             </Description>
-            </ImageForm>
             </ServicesBox>
           </ServicesWrapper>
-          
         </ServicesForm>
         
      );
--- src/Pages/Profile/page/Form/Services/ServicesFormStyle.js
+++ src/Pages/Profile/page/Form/Services/ServicesFormStyle.js
@@ -7,57 +7,76 @@ export const ServicesForm = styled.div`
 `;
 
 export const ServicesWrapper = styled.div`
-   max-width:1320px;
+   max-width:1350px;
    height:805.56px;
    margin:auto;
 `;
 
 export const ServicesBox = styled.div`
- 
-   width:380px;
+   max-width:432px;
    height:350.78px;
-   display:inline-flex;
-   padding:0px 12px 0px 12px;
-   margin:20px 0px 20px 20px;
+   display:inline-block;
+   padding:0px -12px 0px -12px;
+   margin: 40px 3px 0px 3px;
    background:#fff;
    border-radius:10%;
-   
 `;
 
 
 export const ImageForm = styled.div`
-    width:100px;
-    height:100px;
-    margin:20px auto;
-    border:1px solid #000;
-    border-radius:100%;
-    padding:15px;
-    background-color: ${({ onClick }) =>  onClick ? 'red': 'blue'}
-      
-    
+     border:1px solid #000;
+     width:357.41px; 
+     margin:auto;
+     margin-bottom:16px;
+     height:100px;
+ 
     `
     
     export const Img = styled.div`
-    width:100px;
-    height:100px;
+       border:1px solid #000;
+       width:100px;
+       height:100px;
+       margin:auto;
+       border-radius:100%;
+       background:${props => (props.onClick ? 'blue': 'blue')};
+       
+       &:hover {
+          opacity:${props => (props.onClick ? '100%': '100%')};
+          transition:0.5s;
+          background: ${props => (props.onClick ? 'red': 'powderblue')}
+       }
 
-    border:1px solid #fff;
-    border-radius:100%;
-    display:block;
+     
+`;
+
+export const ImageH1 = styled.div` 
 
  
 `;
 
-export const ImageH1 = styled.div`
-   float:left;
-  font:bold 16px/18px arial;
+export const Description = styled.div`
+display:block;   
+border:1px solid #000;
+`
+
+export const Icon = styled.div`
+  width:50px;
+  height:50px;
+  margin:10px auto;
+  padding:15px;
+  background:#fff;
+  border-radius:100%;
+  opacity:100%;
+  &:hover{ 
+  background: ${props => (props.onClick ? 'red': 'blue')}
+  }
+
 `;
 
-export const Description = styled.div`
-   width:371.41px;
-   height:142.78px;
-   margin:30px -80px auto;
+export const Ico = styled.image`
+   width:40px;
+   height:40px;
    position:relative;
-   left:-50px;
-   border:1px solid #000;
+   left:-45px;
+   top:0px;padding:50px;
 `
