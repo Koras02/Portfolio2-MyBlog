2da2ec004432eddc3219b305f858313986e099a3
Koras02 <wjdgh0727@gmail.com>
2021-02-15 18:03:50 +0900

    중간 저장

--- src/Pages/Profile/page/Form/ProfileForm.js
+++ src/Pages/Profile/page/Form/ProfileForm.js
@@ -1,32 +1,52 @@
- import React from 'react';
+ import React, {Component} from 'react';
 import { 
   ProfileContainer,
   ProfileFormWrapper,
   ProflieOverflow,
-  ProfileSwipper,
-  ProfileSwipper1,
+  ProfileForm1,
+  ProfileForm2
+  // ProfileSwipper1,
   // ProfileSwipper2
 } from '../style/ProfileStyle';
 import './Slide.scss';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export function ProfileForm() {
+export default class ProfileForm extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      pauseOnHover: true
+    };
      return (
        <ProfileFormWrapper>
            <ProflieOverflow>
               <ProfileContainer>
-                  <ProfileSwipper className="slide">
-                      <ProfileSwipper1 className="slide">
-                           1번
-                      </ProfileSwipper1>
-                      <ProfileSwipper1>
-                           2번
-                      </ProfileSwipper1>
-                  </ProfileSwipper>
+                <div>
+                <Slider {...settings}>
+                  <ProfileForm1>
+                    <ProfileForm2>1</ProfileForm2>
+                  </ProfileForm1>
+                  <ProfileForm1>
+                  <ProfileForm2>2</ProfileForm2>
+                  </ProfileForm1>
+                  <ProfileForm1>
+                  <ProfileForm2>3</ProfileForm2>
+                  </ProfileForm1>       
+                </Slider>
+              </div>
+            
               </ProfileContainer>
            </ProflieOverflow>
        </ProfileFormWrapper>
      );
  }
+}
 
-
- export default ProfileForm;
+ 
--- src/Pages/Profile/page/style/ProfileStyle.js
+++ src/Pages/Profile/page/style/ProfileStyle.js
@@ -42,40 +42,20 @@ export const ProfileSwipper = styled.div`
    border:1px solid #000;
 `
 
-export const ProfileSwipper1 = styled.div`
-  width:1296px;
+export const ProfileForm1 = styled.div`
+  width:100%;
   height:100%;
   border:1px solid #000;
   display:block;
-  animation-name: slide;
-
-  @keyframes slide {
-    0% {margin-left:0;} /* 0 ~ 10  : 정지 */
-    10% {margin-left:0;} /* 10 ~ 25 : 변이 */
-    25% {margin-left:-100%;} /* 25 ~ 35 : 정지 */
-    35% {margin-left:-100%;} /* 35 ~ 50 : 변이 */
-    50% {margin-left:-200%;}
-    60% {margin-left:-200%;}
-    75% {margin-left:-300%;}
-    85% {margin-left:-300%;}
-    100% {margin-left:0;}
-
-}
-
-  &:active {
-      position:relative;
-      left:${props => props.default ? '50px': '50px'};
-      transition:${props => props.default ? '5s': '10s'};
-  }
-`
-
-export const ProfileSwipper2 = styled.div`
-width:1296px;
-height:100%;
-border:1px solid #000;
-display:none;
  
-&:active {
-
-}
+ 
+    
 `
+
+export const ProfileForm2 = styled.div`
+  max-width:1900px;
+  height:199px;
+  border:1px solid red;
+`
+
+ 
