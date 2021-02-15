f4157464bc5b896ac41da4c5f4b3f2eb7695b510
Koras02 <wjdgh0727@gmail.com>
2021-02-15 23:44:58 +0900

    프로필 추가

--- src/Pages/Profile/page/Form/ProfileForm.js
+++ src/Pages/Profile/page/Form/ProfileForm.js
@@ -4,25 +4,34 @@ import {
   ProfileFormWrapper,
   ProflieOverflow,
   ProfileForm1,
-  ProfileForm2
+  ProfileForm2,
+  ProfileForm3,
+  ProfileFormTab,
+  ProfileImg1,
+  ProfileImg2,
+  ProfileImg3,
   // ProfileSwipper1,
   // ProfileSwipper2
 } from '../style/ProfileStyle';
 import './Slide.scss';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.scss";
 
 export default class ProfileForm extends Component {
   render() {
     var settings = {
       dots: true,
-      infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      pauseOnHover: true
+      autoplaySpeed: 1000,
+      autoPlay:true,
+      pauseOnHover: true,
+      appendDots: dots => (
+        <div>
+             <ul className="slide">{dots}</ul>
+        </div>
+      )
     };
      return (
        <ProfileFormWrapper>
@@ -31,17 +40,39 @@ export default class ProfileForm extends Component {
                 <div>
                 <Slider {...settings}>
                   <ProfileForm1>
-                    <ProfileForm2>1</ProfileForm2>
+                    <ProfileForm2>
+                       <ProfileForm3>
+                         <ProfileFormTab>
+                            <ProfileImg1>
+                            </ProfileImg1>
+                         </ProfileFormTab>
+                       </ProfileForm3>
+                    </ProfileForm2>
                   </ProfileForm1>
                   <ProfileForm1>
-                  <ProfileForm2>2</ProfileForm2>
+                  <ProfileForm2>
+                      <ProfileForm3>
+                      <ProfileFormTab>
+                      <ProfileImg2>
+                              
+                      </ProfileImg2>
+                      </ProfileFormTab>
+                      </ProfileForm3>
+                  </ProfileForm2>
                   </ProfileForm1>
                   <ProfileForm1>
-                  <ProfileForm2>3</ProfileForm2>
+                  <ProfileForm2>
+                  <ProfileForm3>
+                      <ProfileFormTab>
+                      <ProfileImg3>
+                              
+                        </ProfileImg3>
+                      </ProfileFormTab>
+                      </ProfileForm3>
+                  </ProfileForm2>
                   </ProfileForm1>       
                 </Slider>
               </div>
-            
               </ProfileContainer>
            </ProflieOverflow>
        </ProfileFormWrapper>
--- src/Pages/Profile/page/style/ProfileStyle.js
+++ src/Pages/Profile/page/style/ProfileStyle.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import Image1 from '../Form/Images/Mount.jpg'
+import Image2 from '../Form/Images/Profile3.jpg'
+import Image3 from '../Form/Images/Beard.jpg'
 
 export const ProfileFormWrapper = styled.div`
   width:100%;
@@ -23,11 +25,11 @@ opacity:80%;
 `;
 
 export const ProfileContainer = styled.div`
-  max-width:1320px;
+  width:1320px;
   height:351px;
   margin:auto;
   padding:0px 12px 0px 12px;
-  border:1px solid #000;
+ 
   position:relative;
   top:150px;
 `;
@@ -40,22 +42,72 @@ export const ProfileSwipper = styled.div`
    transition-property: transform;
    box-sizing: content-box;
    border:1px solid #000;
+   outline:none;
 `
 
 export const ProfileForm1 = styled.div`
-  width:100%;
+  max-width:1296px;
   height:100%;
-  border:1px solid #000;
   display:block;
- 
- 
+  overflow:hidden;
+  position:relative;
+  outline:none;
+  overflow:none;
     
 `
 
 export const ProfileForm2 = styled.div`
-  max-width:1900px;
-  height:199px;
-  border:1px solid red;
+  width:1296px;
+  height:307px;
+  overflow:hidden;
+  position:relative;
+ 
+`;
+
+export const ProfileForm3 = styled.div`
+   width:1290px;
+   height:307px;
+ 
+`
+ export const ProfileFormTab = styled.div`
+    width:1296px;
+    height:199px;
+    margin-top:16px;
+ 
+ `;
+
+ export const ProfileImg1 = styled.div`
+    width:150px;
+    height:150px;
+    margin:auto;
+    display:block;
+ 
+    border-radius:100%;
+    background:url(${Image2}) no-repeat;
+    background-size:cover;
+ 
+ `;
+
+ export const ProfileImg2 = styled.div`
+ width:150px;
+ height:150px;
+ margin:auto;
+ display:block;
+ border:1px solid #000;
+ border-radius:100%;
+ background:url(${Image3}) no-repeat;
+ background-size:cover;
+
 `
 
- 
+export const ProfileImg3 = styled.div`
+width:150px;
+height:150px;
+margin:auto;
+display:block;
+border:1px solid #000;
+border-radius:100%;
+background:url(${Image2}) no-repeat;
+background-size:cover;
+
+`
